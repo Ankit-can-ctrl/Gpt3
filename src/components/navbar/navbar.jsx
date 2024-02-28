7ad3905627e57b9118ca8b77366ae536ac263f0a
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-02-28 23:16:44 +0530

    navbar completed

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,51 +1,106 @@
 import React, { useState } from "react";
-import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
+const navbarLinks = [
+  "Home",
+  "What is gpt?",
+  "Open AI",
+  "Case Studies",
+  "Library",
+];
 
+function Menu() {
+  return (
+    <div className="navbar_links">
+      <li>
+        <a href="#home">Home</a>
+      </li>
+      <li>
+        <a href="#wgpt3">What is GPT?</a>
+      </li>
+      <li>
+        <a href="#possibility">Open AI</a>
+      </li>
+      <li>
+        <a href="#features">Case Studies</a>
+      </li>
+      <li>
+        <a href="#blog">Library</a>
+      </li>
+    </div>
+  );
+}
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div className="gpt3_navbar">
-      <div className="gpt3_navbar_links">
-        <div className="navbar_logo">
+    <div className="gpt3_navbar_container">
+      <div className="gpt3_navbar">
+        <div className="nav_logo">
           <img src={logo} alt="logo" />
         </div>
-        <div className="navbar_links">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#wgpt3">What is GPT?</a>
-          </li>
-          <li>
-            <a href="#possibility">Open AI</a>
-          </li>
-          <li>
-            <a href="#features">Case Studies</a>
-          </li>
-          <li>
-            <a href="#blog">Library</a>
-          </li>
+        <div className="nav_links">
+          {navbarLinks.map((item, index) => (
+            <a href={item} key={item}>
+              {item}
+            </a>
+          ))}
         </div>
-        <div className="navbar_btns">
-          <button className="navbar_signin_btn">Sign In</button>
-          <button className="navbar_signup_btn">Sign Up</button>
-        </div>
-        <div className="navbar_toggle_menu">
-          {toggleMenu ? (
-            <RiCloseLine
-              color="#fff"
-              size={27}
-              onClick={() => setToggleMenu(false)}
-            />
-          ) : (
-            <RiMenu3Line
-              color="#fff"
-              size={27}
-              onClick={() => setToggleMenu(false)}
-            />
-          )}
+        <div className="nav_end_part">
+          <div className="nav_btns">
+            <button className="nav_in_btn">Sign In</button>
+            <button className="nav_up_btn">Sign Up</button>
+          </div>
+          <div className="nav_toggle_menu">
+            {toggleMenu ? (
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                // stroke-width="0"
+                viewBox="0 0 24 24"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={() => setToggleMenu(false)}
+                color="#fff"
+                fontSize={27}
+              >
+                <path
+                  fill="none"
+                  // stroke="#000"
+                  // stroke-width="2"
+                  d="M7,7 L17,17 M7,17 L17,7"
+                ></path>
+              </svg>
+            ) : (
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                // stroke-width="0"
+                viewBox="0 0 24 24"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={() => setToggleMenu(true)}
+                color="#fff"
+                fontSize={27}
+              >
+                <path d="M4 6H20V8H4zM4 11H20V13H4zM4 16H20V18H4z"></path>
+              </svg>
+            )}
+            {toggleMenu && (
+              <div className="nav_ontoggle">
+                {navbarLinks.map((item, index) => (
+                  <a href={item} key={item}>
+                    {item}
+                  </a>
+                ))}
+                <button className="nav_in_btn up_in_btn">Sign In</button>
+                <button className="nav_up_btn up_in_btn">Sign Up</button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
