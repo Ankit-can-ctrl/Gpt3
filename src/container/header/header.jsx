330cb0df55f5f5b6aaf85ff89d955a68673d7ae3
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-02-29 23:53:37 +0530

    creating the header for mobile devices

--- src/container/header/header.jsx
+++ src/container/header/header.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 export default function Header() {
   return (
-    <div className="Header">
-      <h1>Header</h1>
+    <div className="header_container">
+      <div className="top_text">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Do not go where the path may lead, go instead where there is no path
+          and leave a trail. -Ralph Waldo Emerson
+        </p>
+      </div>
+      <div className="input_part">
+        <input
+          className="input"
+          type="email"
+          placeholder="Your Email Address"
+        />
+        <button type="button">Get Started</button>
+      </div>
+      <div className="people_part">
+        <img src={people} alt="people" />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+      <div className="ai_img">
+        <img src={ai} alt="ai" />
+      </div>
     </div>
   );
 }
