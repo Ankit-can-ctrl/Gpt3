513119929ee352edfe7ededf1d7faa84daaba7ea
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-03-01 18:45:01 +0530

    completed the brands component

--- src/components/brand/brand.jsx
+++ src/components/brand/brand.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./brand.css";
+import { slack, atlassian, google, shopify, dropbox } from "./import";
 
 export default function Brand() {
   return (
-    <div className="Brand">
-      <h1>brand</h1>
+    <div className="brand_container">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
     </div>
   );
 }
