51d96c05896a998b81ae735d9edbb678945df6d1
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-03-03 20:53:04 +0530

    cta component completed

--- src/components/cta/cta.jsx
+++ src/components/cta/cta.jsx
@@ -3,8 +3,14 @@ import "./cta.css";
 
 export default function CTA() {
   return (
-    <div className="CTA">
-      <h1>Cta</h1>
+    <div className="cta_main_container">
+      <div className="cta_content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="cta_btn">
+        <button type="button">Get Started</button>
+      </div>
     </div>
   );
 }
