87685f268f82a88ce6edfc55d7ea41f52aa478a4
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-03-04 13:20:51 +0530

    completed blog component

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./article.css";
 
-export default function Article() {
+export default function Article({ imgUrl, date, text }) {
   return (
-    <div className="Article">
-      <h1>Article</h1>
-    </div>
+    <>
+      <div className="article_main_container">
+        <div className="article_img">
+          <img src={imgUrl} alt="blog" />
+        </div>
+        <div className="article_text">
+          <div className="article_header">
+            <p>{date}</p>
+            <h3>{text}</h3>
+          </div>
+          <p>
+            Read Full Article
+            <span className="arrow">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 1024 1024"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"></path>
+              </svg>
+            </span>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
--- src/container/blog/blog.jsx
+++ src/container/blog/blog.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import "./blog.css";
+import Article from "../../components/article/article";
+import { blog01, blog02, blog03, blog04, blog05 } from "./import";
 
 export default function Blog() {
   return (
-    <div className="Blog">
-      <h1>Blog</h1>
+    <div className="blog_main_container">
+      <div className="blog_heading">
+        <h1 className="gradient__text">
+          A lot is happening, <br /> We are blogging about it.
+        </h1>
+      </div>
+      <div className="blog_articles">
+        <div className="blog_main_article">
+          <Article
+            imgUrl={blog01}
+            date="Sep 26,2011"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+        <div className="blog_sec_article">
+          <Article
+            imgUrl={blog02}
+            date="Sep 26,2011"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog03}
+            date="Sep 26,2011"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog04}
+            date="Sep 26,2011"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog05}
+            date="Sep 26,2011"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+      </div>
     </div>
   );
 }
